b4d03c048ad9ae025e1b17c31aeb36808f97502a
njb_said <njb_said@hotmail.co.uk>
2015-04-17 22:57:34 +0100

    Added home and parser
    The homepage is.. going to change (along with the design) :wink:

---
+++ docs/home.md
@@ -0,0 +1,22 @@
+# <span id="intro">Combat Stats</span>
+CombatStats is a powerful stats plugin.
+
+## Table of Contents
+- [Introduction](#intro)
+- [Config](/config)
+- [API](/api)
+
+By default it has the following statistics (all toggleable via config)
+
+- Player Kills
+- Mob Kills
+- Deaths
+- Projectiles Launched
+- Highest Player Killstreak
+- Blocks Broken
+- Blocks Placed
+- Items Crafted
+- Total play time
+
+Again, all of the stats listed above can be disabled or enabled at your own discretion using the config. If you have a suggestion please let me know and I will try to add it.
+You are not limited to me though on what you can do this with plugin, you can use the plugin's API to add your own stats (see [API docs](/api) for more info)
---
+++ route.php
@@ -0,0 +1,33 @@
+<?php
+include_once(__DIR__ . "/Parsedown.php");
+
+$uri = strtolower(rtrim($_SERVER["REQUEST_URI"]));
+$request = explode('/', $uri);
+array_shift($request);
+$first = removeGet($request[0]);
+$base = "/docs/";
+
+if($first == '') {
+    showFile("home.md");
+} else {
+    showFile($first . ".md");
+}
+
+function showFile($path) {
+    global $base;
+    if(file_exists(__DIR__ . $base . $path) && is_readable(__DIR__ . $base . $path)) {
+        $Parsedown = new Parsedown();
+        $split = explode(".", $path);
+        echo str_replace("{{page_title}}", ucwords(str_replace("_", " ", $split[0])), str_replace("{{page_content}}", $Parsedown->text(file_get_contents(__DIR__ . $base . $path)), file_get_contents(__DIR__ . "/template.html")));
+    } else {
+        header("HTTP/1.0 404 Not Found");
+        header("Content-type: text/plain");
+        echo 'Input file not found: ' . URI;
+        // TODO: a proper 404 page
+    }
+    die();
+}
+
+function removeGet($url) {
+    return preg_replace('/\\?.*/', '', $url);
+}
